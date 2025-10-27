d848bc2f08604f464262a796fb8392d07a6512a5
ruiwei.guo <ruiwei.guo@okg.com>
2025-10-28 00:24:36 +0800

    modify mainnet config

--- scripts/rpc-setup/one-click-setup.sh
+++ scripts/rpc-setup/one-click-setup.sh
@@ -36,13 +36,11 @@ TESTNET_ROLLUP_CONFIG="rollup-testnet.json"
 TESTNET_GETH_CONFIG="op-geth-config-testnet.toml"
 
 # Mainnet configuration
-# TODO: fix config
 MAINNET_BOOTNODE_OP_NODE="enode://c67d7f63c5483ab8311123d2997bfe6a8aac2b117a40167cf71682f8a3e37d3b86547c786559355c4c05ae0b1a7e7a1b8fde55050b183f96728d62e276467ce1@8.210.177.150:9223,enode://28e3e305b266e01226a7cc979ab692b22507784095157453ee0e34607bb3beac9a5b00f3e3d7d3ac36164612ca25108e6b79f75e3a9ecb54a0b3e7eb3e097d37@8.210.15.172:9223,enode://b5aa43622aad25c619650a0b7f8bb030161dfbfd5664233f92d841a33b404cea3ffffdc5bc8d6667c7dc212242a52f0702825c1e51612047f75c847ab96ef7a6@8.210.69.97:9223"
 MAINNET_P2P_STATIC="/ip4/47.242.38.0/tcp/9223/p2p/16Uiu2HAmH1AVhKWR29mb5s8Cubgsbh4CH1G86A6yoVtjrLWQgiY3,/ip4/8.210.153.12/tcp/9223/p2p/16Uiu2HAkuerkmQYMZxYiQYfQcPob9H7XHPwS7pd8opPTMEm2nsAp,/ip4/8.210.117.27/tcp/9223/p2p/16Uiu2HAmQEzn2WQj4kmWVrK9aQsfyQcETgXQKjcKGrTPsKcJBv7a"
 MAINNET_OP_STACK_IMAGE="xlayer/op-node:0.0.9"
 MAINNET_OP_GETH_IMAGE="xlayer/op-geth:0.0.6"
-# TODO: fix url
-MAINNET_GENESIS_URL="https://okg-pub-hk.oss-cn-hongkong.aliyuncs.com/cdn/chain/xlayer/snapshot/merged.genesis.json.test.tar.gz"
+MAINNET_GENESIS_URL="https://okg-pub-hk.oss-cn-hongkong.aliyuncs.com/cdn/chain/xlayer/snapshot/merged.genesis.json.mainnet.tar.gz"
 MAINNET_SEQUENCER_HTTP="https://rpc.xlayer.tech"
 MAINNET_ROLLUP_CONFIG="rollup-mainnet.json"
 MAINNET_GETH_CONFIG="op-geth-config-mainnet.toml"
