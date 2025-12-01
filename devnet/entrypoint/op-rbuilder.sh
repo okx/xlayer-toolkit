f662c7fdd3b693705721c40db57c0ebab9347587
lucas.lim <lucas.lim@okg.com>
2025-12-01 18:00:58 +0800

    fix

--- devnet/entrypoint/op-rbuilder.sh
+++ devnet/entrypoint/op-rbuilder.sh
@@ -7,7 +7,7 @@ source /.env
 # Enable jemalloc profiling if requested
 # Note: tikv-jemalloc (used by Rust) uses _RJEM_MALLOC_CONF, not MALLOC_CONF
 if [ "${JEMALLOC_PROFILING:-false}" = "true" ]; then
-    export _RJEM_MALLOC_CONF="prof:true,prof_prefix:/profiling/jeprof,lg_prof_sample:21"
+    export _RJEM_MALLOC_CONF="prof:true,prof_prefix:/profiling/jeprof,lg_prof_interval:30"
     echo "Jemalloc profiling enabled: _RJEM_MALLOC_CONF=$_RJEM_MALLOC_CONF"
 fi
 
