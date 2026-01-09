61938873d5a52b46065b6775889426ad5e425353
Niven <sieniven@gmail.com>
2026-01-09 10:46:27 +0800

    Add flashblock subscription flag in devnet reth rpc node (#86)

--- devnet/entrypoint/reth-rpc.sh
+++ devnet/entrypoint/reth-rpc.sh
@@ -56,7 +56,8 @@ CMD="op-reth node \
 # For flashblocks architecture. Enable flashblocks RPC
 if [ "$FLASHBLOCK_ENABLED" = "true" ] && [ "$FLASHBLOCKS_RPC" = "true" ]; then
     CMD="$CMD \
-        --flashblocks-url=ws://op-reth-seq:1111"
+        --flashblocks-url=ws://op-reth-seq:1111 \
+        --xlayer.flashblocks-subscription"
 fi
 
 exec $CMD
