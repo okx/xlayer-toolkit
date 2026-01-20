a80cdaa17162fad670420080b7ed2d0599d985cb
brendontan03 <brendontan003@gmail.com>
2026-01-20 11:40:50 +0800

    fix docker configs

--- devnet/entrypoint/reth-seq.sh
+++ devnet/entrypoint/reth-seq.sh
@@ -11,7 +11,7 @@ if [ "${JEMALLOC_PROFILING:-false}" = "true" ]; then
     echo "Jemalloc profiling enabled: _RJEM_MALLOC_CONF=$_RJEM_MALLOC_CONF"
 fi
 
-exec op-reth node \
+CMD="op-reth node \
       --datadir=/datadir \
       --chain=/genesis.json \
       --config=/config.toml \
@@ -32,7 +32,7 @@ exec op-reth node \
       --authrpc.addr=0.0.0.0 \
       --authrpc.port=8552 \
       --authrpc.jwtsecret=/jwt.txt \
-      --trusted-peers="${TRUSTED_PEERS}" \
+      --trusted-peers=$TRUSTED_PEERS \
       --tx-propagation-policy=all \
       --txpool.max-account-slots=100000 \
       --txpool.pending-max-count=100000 \
@@ -40,39 +40,37 @@ exec op-reth node \
       --txpool.basefee-max-count=100000 \
       --txpool.max-pending-txns=100000 \
       --txpool.max-new-txns=100000 \
-      --txpool.pending-max-size=2000 \
-      --txpool.basefee-max-size=2000 \
       --engine.persistence-threshold=${ENGINE_PERSISTENCE_THRESHOLD:-2} \
       --log.file.directory=/logs/reth \
       --log.file.filter=info \
       --metrics=0.0.0.0:9001"
 
 
-# For flashblocks architecture
-if [ "$FLASHBLOCK_ENABLED" = "true" ]; then
-    CMD="$CMD \
-        --flashblocks.enabled \
-        --flashblocks.disable-rollup-boost \
-        --flashblocks.disable-state-root \
-        --flashblocks.addr=0.0.0.0 \
-        --flashblocks.port=1111 \
-        --flashblocks.block-time=200"
+# # For flashblocks architecture
+# if [ "$FLASHBLOCK_ENABLED" = "true" ]; then
+#     CMD="$CMD \
+#         --flashblocks.enabled \
+#         --flashblocks.disable-rollup-boost \
+#         --flashblocks.disable-state-root \
+#         --flashblocks.addr=0.0.0.0 \
+#         --flashblocks.port=1111 \
+#         --flashblocks.block-time=200"
 
-    if [ "$FLASHBLOCK_P2P_ENABLED" = "true" ] && [ "$CONDUCTOR_ENABLED" = "true" ]; then
-        CMD="$CMD \
-            --flashblocks.p2p_enabled \
-            --flashblocks.p2p_port=9009 \
-            --flashblocks.p2p_private_key_file=/datadir/fb-p2p-key"
+#     if [ "$FLASHBLOCK_P2P_ENABLED" = "true" ] && [ "$CONDUCTOR_ENABLED" = "true" ]; then
+#         CMD="$CMD \
+#             --flashblocks.p2p_enabled \
+#             --flashblocks.p2p_port=9009 \
+#             --flashblocks.p2p_private_key_file=/datadir/fb-p2p-key"
 
-        INDEX="${1:-}"
-        if [ -z "$INDEX" ]; then
-            # op-reth-seq connects to op-reth-seq2
-            CMD="$CMD --flashblocks.p2p_known_peers=/dns4/op-reth-seq2/tcp/9009/p2p/12D3KooWGnxtRXJWhNtwKmRjpqj5QFQPskjWJkC7AkGWhCXBM6ed"
-        else
-            # op-reth-seq2 connects to op-reth-seq
-            CMD="$CMD --flashblocks.p2p_known_peers=/dns4/op-reth-seq/tcp/9009/p2p/12D3KooWC6qFQzcS6V6Tp53nRqw2pmU1snjSYq7H4Q6ckTWAskTt"
-        fi
-    fi
-fi
+#         INDEX="${1:-}"
+#         if [ -z "$INDEX" ]; then
+#             # op-reth-seq connects to op-reth-seq2
+#             CMD="$CMD --flashblocks.p2p_known_peers=/dns4/op-reth-seq2/tcp/9009/p2p/12D3KooWGnxtRXJWhNtwKmRjpqj5QFQPskjWJkC7AkGWhCXBM6ed"
+#         else
+#             # op-reth-seq2 connects to op-reth-seq
+#             CMD="$CMD --flashblocks.p2p_known_peers=/dns4/op-reth-seq/tcp/9009/p2p/12D3KooWC6qFQzcS6V6Tp53nRqw2pmU1snjSYq7H4Q6ckTWAskTt"
+#         fi
+#     fi
+# fi
 
 exec $CMD
